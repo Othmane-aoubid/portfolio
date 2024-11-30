702d5b504389fdb439e6306a56060fe64aa7328e
othmane_aoubid <othmane.aoubid.yosobox@gmail.com>
2024-11-30 21:09:29 +0000

    added vercel analytics

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import './index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 const app = createApp(App)
 
